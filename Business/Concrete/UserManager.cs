14aad8709df456ee5f290f83d59f70858e6fa69d
Uğur Koçak <muhammedugr16@gmail.com>
2023-08-26 15:31:06 +0300

    Add Autofac IoC Service Add FluentValidation Add ValidationAspect

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilites.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,7 +21,7 @@ namespace Business.Concrete
         {
             _userDal = userDal;
         }
-
+        [ValidationAspect(typeof(UserValidator))]
         public IResult Add(User user)
         {
             if (user.FirstName.Length < 2 && user.LastName.Length < 2)
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,9 @@
 
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -15,19 +18,25 @@ namespace WebAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
-            builder.Services.AddSingleton<ICarService,CarManager>();
-            builder.Services.AddSingleton<ICarDal,EfCarDal>();
-            builder.Services.AddSingleton<IBrandService,BrandManager>();
-            builder.Services.AddSingleton<IBrandDal,EfBrandDal>();
-            builder.Services.AddSingleton<IColorService,ColorManager>();
-            builder.Services.AddSingleton<IColorDal,EfColorDal>();
-            builder.Services.AddSingleton<IRentalService,RentalManager>();
-            builder.Services.AddSingleton<IRentalDal,EfRentalDal>();
-            builder.Services.AddSingleton<IUserService,UserManager>();
-            builder.Services.AddSingleton<IUserDal,EfUserDal>();
-            builder.Services.AddSingleton<ICustomerService,CustomerManager>();
-            builder.Services.AddSingleton<ICustomerDal,EfCustomerDal>();
+            //builder.Services.AddSingleton<ICarService,CarManager>();
+            //builder.Services.AddSingleton<ICarDal,EfCarDal>();
+            //builder.Services.AddSingleton<IBrandService,BrandManager>();
+            //builder.Services.AddSingleton<IBrandDal,EfBrandDal>();
+            //builder.Services.AddSingleton<IColorService,ColorManager>();
+            //builder.Services.AddSingleton<IColorDal,EfColorDal>();
+            //builder.Services.AddSingleton<IRentalService,RentalManager>();
+            //builder.Services.AddSingleton<IRentalDal,EfRentalDal>();
+            //builder.Services.AddSingleton<IUserService,UserManager>();
+            //builder.Services.AddSingleton<IUserDal,EfUserDal>();
+            //builder.Services.AddSingleton<ICustomerService,CustomerManager>();
+            //builder.Services.AddSingleton<ICustomerDal,EfCustomerDal>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Host
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>(builder =>
+                {
+                    builder.RegisterModule(new AutofacBusinessModule());
+                });
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
